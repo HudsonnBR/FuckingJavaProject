419223e46222e07c8b16bd8b7eb2af8e2a0f6ab0
Hudsonn <hudson.gatoh@gmail.com>
2017-04-04 09:36:16 -0300

    Batatox

--- src/java/br/edu/projecao/model/AtorDao.java
+++ src/java/br/edu/projecao/model/AtorDao.java
@@ -12,14 +12,34 @@ public class AtorDao {
         Connection conn = null;
         PreparedStatement ps = null;
         try{
+            //Pega a Conexao feita...
             conn = Conexao.conectarBanco();
+            // Pré Compila o SQL que vai pro banco
             ps = conn.prepareStatement(Ator.INSERE);
+            //Passa os parametros de acordo com a qtd de chaves "?" do SQL...
+            ps.setString (1, obj.getNome());
+            //Executa no BD...
+            int i = ps.executeUpdate();
+            
+            if(i == 1)
+                return "Inserido com sucesso.";
         }
         catch(Exception e){
-            
+            System.out.println("Erro ao inserir Ator");
+            System.out.println(e.getMessage());
         }
         finally{
-            
+            if (conn != null) conn = null;
+            if (ps != null) conn = null;
         }
+        //Não quero que chegue até aqui!
+        return null;
     }
+    /*
+    public static void main(String[] args){
+        Ator a = new Ator(null, "DONALD TRUMP");
+        AtorDao dao = new AtorDao();
+        String msg = dao.insere(a);
+        System.out.println("msg");
+    }*/
 }
